15fca9928cb3587b11439df6b91de3036c46475f
kmcquiter <karlishamcquitter@gmail.com>
2020-04-11 08:40:33 -0400

    Worked on addItem () and getTotal()

---
+++ solution.js
@@ -0,0 +1,66 @@
+////////////// Part 1 /////////////////////
+/* 1.1) Link the CSS file by setting the href attribute of <link> */
+document.querySelector("link").href ="styles.css";
+
+/* 1.2) Set the class of the image to "list-img" */
+
+document.querySelector("img").className = "list-img";
+
+/* 1.3) Set the classes of the inputs (buttons) to "btn" by using a loop */
+
+let listBtn = document.querySelectorAll("input");
+
+for (let i = 0; i < listBtn.length; i++) {
+
+        listBtn[i].className = "btn";
+
+}
+
+
+////////////// Part 2 /////////////////////
+
+/* 2.1) grab the <ul> that we are going to append <li> nodes to and store it in a variable */
+
+let groceryList = document.querySelector("#grocery-list");
+
+
+/* 2.2) When the user clicks the ADD button, ask them what item they need to add and then:
+        1. Create a new li Element
+        2. Create the text node with the user's input
+        3. Append the text node to the li Element
+        4. Add the li Element to the unordered list by appending it to the variable created in step 2.1 */
+
+
+        function addItem() {
+
+              let add = prompt("What item do you want to add?:");
+                console.log(add);
+          
+                let element = document.createElement("li");
+                let textNode = document.createTextNode(add);
+                element.appendChild(textNode);
+                groceryList.appendChild(element);
+              }
+
+
+/* 2.3) When the user clicks the TOTAL button:
+        1. Display the total area by setting the total-area class to just "total-area"
+        2. Total the number of groceries by getting the length of the li NodeList
+        3. Add the count to the page by setting the textContent of #item-count */
+
+
+              function getTotal(){
+
+                let total = document.querySelector(".total-area");
+                console.log(total);
+
+                let groceryList = document.querySelectorAll("#grocery-list");
+                let numOfGroceries = groceryList.length;
+                document.querySelectorAll("#item-count").textContent = numOfGroceries;
+                }
+              
+
+////////////// BONUS /////////////////////
+/*  When the user clicks the REMOVE button, ask them what item they want to remove and then:
+    1. Remove the item from the list with removeChild by comparing the user's response to the textContent of each <li> */
+
